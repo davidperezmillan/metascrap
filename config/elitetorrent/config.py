9bbc3154ed5dfcf01957bbe3c27407bd1337853d
David Perez Millan <david@davidperezmillan.com>
2017-10-16 08:50:10 +0000

    elimino archivos innecesarios

--- config/elitetorrent/config.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-file_log = "log/elitetorrent//log_file.log"
--- config/newpct1/config.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-file_log = "log/newpct1/log_file.log"
